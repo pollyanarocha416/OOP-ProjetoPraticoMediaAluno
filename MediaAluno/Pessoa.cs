16ac60eb9d76dc0039cba84fe5e377681545b8c6
Pollyana <pollyanarocha416@gmail.com>
2022-05-25 11:07:25 -0300

    Adicionar arquivos de projeto.

---
+++ MediaAluno/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Pessoa
+{
+    // Atributos
+    public string nome;
+    public double nota1, nota2, nota3, nota4;
+
+
+    // Média
+    public double media()
+    {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+
+
+    // Situação
+    public string situacao(double media)
+    {
+        return media >= 7 ? "aprovado" : "reprovado";
+    }
+
+    // Mensagem
+    public void mensagem()
+    {
+        // Obter a média
+        double obterMedia = media();
+
+        // Obter a situação
+        string obterSituacao = situacao(obterMedia);
+
+        // Mensagem
+        Console.WriteLine(nome + " está " + obterSituacao + " com média " + obterMedia);
+    }
+
+}
+
---
+++ MediaAluno/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estanciar Objt das classes presentes
+            Pessoa pssa = new Pessoa();
+            pssa.nome = "Fabio";
+            pssa.nota1 = 6;
+            pssa.nota2 = 3;
+            pssa.nota3 = 9;
+            pssa.nota4 = 7;
+            pssa.mensagem();
+
+        }
+    }
+}
